dca30b741201b872b267c0da6a101b74a4f5f2bd
github username <github email address>
2019-04-13 15:50:22 -0400

    added skeleton components

---
+++ src/components/CounselorProfile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default class CounselorProfile extends React.Component {
+    render() {
+        return (
+            <div class="ui grid centered">
+                <div class="ui segment">
+                    <section>counselor name</section>
+                    <section> More Info: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum eu facilisis sed odio morbi quis commodo odio aenean. Duis tristique sollicitudin nibh sit amet commodo. At varius vel pharetra vel turpis nunc eget. Purus in massa tempor nec. Amet volutpat consequat mauris nunc congue nisi vitae. Elementum eu facilisis sed odio morbi quis.</section>
+                    <section>age: </section><section>gender:</section>
+                    <section>Email: </section>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostCard from './PostCard'
+
+const PostList = (props) => {
+    return (
+        <div>
+        //list of posts goes here {<PostCard/>}
+        </div>
+    )
+}
+
+export default PostList
