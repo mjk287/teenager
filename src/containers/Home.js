f11fbfc7c56901464318a2cfd37e8376259321ed
Maximo Bautista <max.bautista97@gmail.com>
2019-04-13 16:50:26 -0400

    Added routes and update app.js

---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+const Home = props => {
+  console.log(props.user);
+  return (
+    <div>
+      {Object.keys(props.user).length > 0 ? (
+        <h1>Home</h1>
+      ) : (
+        <Redirect to="/login" />
+      )}
+    </div>
+  );
+};
+
+export default Home;
